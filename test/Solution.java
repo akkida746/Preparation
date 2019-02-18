c4c27d4057c8d3139510b0294fafa0041a86fd5a
Akki <akkida746@gmail.com>
2019-02-19 01:00:04 +0530

    Update Solution.java

--- test/Solution.java
+++ test/Solution.java
@@ -1,5 +1,55 @@
 class Solution{
     public static void main(String... args){
         System.out.print("Akash");
+        
+        LinkedList L = new LinkedList(); 
+  
+        /* Let us create the following linked list 
+            5 -> 10 -> 19 -> 28 
+            |    |     |     | 
+            V    V     V     V 
+            7    20    22    35 
+            |          |     | 
+            V          V     V 
+            8          50    40 
+            |                | 
+            V                V 
+            30               45 
+        */
+  
+        L.head = L.push(L.head, 30); 
+        L.head = L.push(L.head, 8); 
+        L.head = L.push(L.head, 7); 
+        L.head = L.push(L.head, 5); 
+  
+        L.head.right = L.push(L.head.right, 20); 
+        L.head.right = L.push(L.head.right, 10); 
+  
+        L.head.right.right = L.push(L.head.right.right, 50); 
+        L.head.right.right = L.push(L.head.right.right, 22); 
+        L.head.right.right = L.push(L.head.right.right, 19); 
+  
+        L.head.right.right.right = L.push(L.head.right.right.right, 45); 
+        L.head.right.right.right = L.push(L.head.right.right.right, 40); 
+        L.head.right.right.right = L.push(L.head.right.right.right, 35); 
+        L.head.right.right.right = L.push(L.head.right.right.right, 20); 
+  
+    }
+    
+    static class LinkedList {
+
+    Node head;  // head of list 
+  
+    /* Linked list Node*/
+    class Node 
+    { 
+        int data; 
+        Node right, down; 
+        Node(int data) 
+        { 
+            this.data = data; 
+            right = null; 
+            down = null; 
+        } 
     }
 }
